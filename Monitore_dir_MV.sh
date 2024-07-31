212ce214f6ce195f17e4b7dbd807f801b5046319
DY <52644394+Dsy000@users.noreply.github.com>
2024-07-31 16:12:56 +0530

    Update Monitore_dir_MV.sh

--- Monitore_dir_MV.sh
+++ Monitore_dir_MV.sh
@@ -22,3 +22,33 @@ do
 done
 
 #V2 ---------------------------------------------------------------------------------------------------------------------
+Move file as per extention 
+#!/bin/bash
+SOURCE_DIR="/mnt/jail/dir1"
+DESTINATION_DIR="/mnt/jail/dir2"
+CSV_DIR="/mnt/jail/dir/csv"
+TXT_DIR="/mnt/jail/dir/txt"
+
+if ! command -v inotifywait &> /dev/null; then
+    echo "inotify-tools not found. Installing..." 
+fi
+inotifywait -m -e close_write --format '%f' "$SOURCE_DIR" | while read NEW_FILE
+do
+    while lsof | grep "$SOURCE_DIR/$NEW_FILE" &> /dev/null
+    do
+        sleep 1
+    done
+    sleep 5
+    if [[ "$NEW_FILE" == *.csv ]]; then
+         DESTINATION_DIR="$CSV_DIR"
+    elif [[ "$NEW_FILE" == *.txt ]]; then
+         DESTINATION_DIR="$TXT_DIR"
+    else
+         echo "Unsupported file type: $NEW_FILE"
+         continue
+    fi
+    currenttime="`date +%y%m%d%H%M%S`_"
+    new_name="$currenttime$NEW_FILE"
+    mv "$SOURCE_DIR/$NEW_FILE" "$DESTINATION_DIR/$new_name"
+    echo "Moved $NEW_FILE to $DESTINATION_DIR/$new_name"
+done
